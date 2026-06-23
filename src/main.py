237c5cddda4cb54d0fbb6901c92211f7e2f3df64
Simeon Pennisi <simeonpennisi@gmail.com>
2026-06-22 22:56:24 -0400

    Added Moon orbit to simulation

--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
-from simulation import integrate_earth_sun
+from simulation import integrate_earth_sun, integrate_earth_moon_sun
 from visualization import plot_trajectory
 
 if __name__ == "__main__":
     earth_positions = integrate_earth_sun(tmax=1, dt=1e-3)
 
+    print("Earth-Sun simulation:")
     print(earth_positions[:10])
     print(earth_positions.shape)
 
     plot_trajectory(earth_positions, "orbit_earth_only.png")
     print("Saved plot to orbit_earth_only.png")
+
+    earth_positions, moon_positions = integrate_earth_moon_sun(tmax=1, dt=1e-3)
+
+    print("Earth-Moon-Sun simulation:")
+    print("Earth:", earth_positions.shape)
+    print("Moon:", moon_positions.shape)
+
+    plot_trajectory(earth_positions, "orbit_earth_moon_earth.png")
+    plot_trajectory(moon_positions, "orbit_earth_moon_moon.png")
+
+    print("Saved Earth and Moon plots")
--- src/simulation.py
+++ src/simulation.py
@@ -13,6 +13,7 @@ def integrate_earth_sun(tmax, dt=1e-3):
     earth_positions = [r_earth.copy()]
 
     for _ in times:
+        # force between two bodies
         force = gravitational_force(
             r_earth,
             r_sun,
@@ -29,3 +30,91 @@ def integrate_earth_sun(tmax, dt=1e-3):
 
     return np.array(earth_positions)
 
+# def integrate_moon_earth(tmax, dt=1e-3):
+#     times = np.arange(0, tmax, dt)
+
+#     r_moon = R0["moon"].copy()
+#     v_moon = V0["moon"].copy()
+#     r_earth = R0["earth"].copy()
+
+#     moon_positions = [r_moon.copy()]
+
+#     for _ in times:
+#         # force between two bodies
+#         force = gravitational_force(
+#             r_moon,
+#             r_earth,
+#             MASS["moon"],
+#             MASS["earth"]
+#         )
+
+#         acceleration = force / MASS["moon"]
+
+#         r_moon = r_moon + v_moon * dt
+#         v_moon = v_moon + acceleration * dt
+
+#         moon_positions.append(r_moon.copy())
+
+#     return np.array(moon_positions)
+
+def integrate_earth_moon_sun(tmax, dt=1e-3):
+    times = np.arange(0, tmax, dt)
+
+    r_sun = R0["sun"].copy()
+
+    r_earth = R0["earth"].copy()
+    v_earth = V0["earth"].copy()
+
+    r_moon = R0["moon"].copy()
+    v_moon = V0["moon"].copy()
+
+    earth_positions = [r_earth.copy()]
+    moon_positions = [r_moon.copy()]
+
+    for _ in times:
+        force_earth_from_sun = gravitational_force(
+            r_earth,
+            r_sun,
+            MASS["earth"],
+            MASS["sun"]
+        )
+
+        force_earth_from_moon = gravitational_force(
+            r_earth,
+            r_moon,
+            MASS["earth"],
+            MASS["moon"]
+        )
+
+        force_moon_from_sun = gravitational_force(
+            r_moon,
+            r_sun,
+            MASS["moon"],
+            MASS["sun"]
+        )
+
+        force_moon_from_earth = gravitational_force(
+            r_moon,
+            r_earth,
+            MASS["moon"],
+            MASS["earth"]
+        )
+
+        earth_acceleration = (
+            force_earth_from_sun + force_earth_from_moon
+        ) / MASS["earth"]
+
+        moon_acceleration = (
+            force_moon_from_sun + force_moon_from_earth
+        ) / MASS["moon"]
+
+        r_earth = r_earth + v_earth * dt
+        r_moon = r_moon + v_moon * dt
+
+        v_earth = v_earth + earth_acceleration * dt
+        v_moon = v_moon + moon_acceleration * dt
+
+        earth_positions.append(r_earth.copy())
+        moon_positions.append(r_moon.copy())
+
+    return np.array(earth_positions), np.array(moon_positions)
